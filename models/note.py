ff71ef9caeacbc898b0a635a1aa98a8d0cc769ac
Raphi04 <raphaelcadete04@gmail.com>
2026-06-24 12:24:28 +0200

    Corrections

--- models/note.py
+++ models/note.py
@@ -170,15 +170,17 @@ def getAvgNoteAnimal(idAnimal):
     myCursor = myDb.cursor()
 
     # On récupère les informations de la note
-    sql = f'''SELECT fiches_animal.idAnimal, fiches_animal.name, AVG(note)
-    FROM notes 
-    JOIN fiches_animal ON fiches_animal.idAnimal = notes.idAnimal
-    WHERE {idAnimal} = {idAnimal}'''
+    sql = f'''
+        SELECT fiches_animal.idAnimal, fiches_animal.name, AVG(note)
+        FROM notes 
+        JOIN fiches_animal ON fiches_animal.idAnimal = notes.idAnimal
+        WHERE {idAnimal} = {idAnimal}
+        '''
     myCursor.execute(sql)
     data = myCursor.fetchall()
 
-    if(data):
-        moyenne_clamp = round(data[0][2],2)
+    if(data[0]):
+        moyenne_clamp = round(data[0][2], 2)
         response = {
             "note": {
                 "idAnimal" : data[0][0],
