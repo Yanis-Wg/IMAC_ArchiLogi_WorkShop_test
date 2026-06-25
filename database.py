39ec308f5bc0f2845c155415f1bbfd702ab7e4d7
Raphi04 <raphaelcadete04@gmail.com>
2026-06-25 09:17:27 +0200

    Ajout du.env

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
+import os
+from dotenv import load_dotenv
+
 import mysql.connector
+
+load_dotenv()
 
 def connectToDB() :
     myDb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="ryj"
+        host=os.getenv("HOST"),
+        user=os.getenv("USER"),
+        password=os.getenv("PWD"),
+        database=os.getenv("DATABASE")
     )
 
     return myDb
